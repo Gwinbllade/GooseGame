0793ec3747eafdedad0e0ff4af2ae8ff2b52822d
PC\ihor <igorbickovskij051@gmail.com>
2024-01-11 19:33:47 +0200

    Added a player and parameters for him

--- main.py
+++ main.py
@@ -6,11 +6,38 @@ pygame.init()
 HEIGHT = 800
 WIDTH = 1200
 
-main_display = pygame.display.set_mode(HEIGHT, WIDTH)
+FPS = pygame.time.Clock()
+
+COLOR_WITHE = (255, 255, 255)
+BLACK  = (0,0,0)
+
+
+main_display = pygame.display.set_mode((WIDTH, HEIGHT))
+
+player_size = (20, 20)
+player = pygame.Surface(player_size)
+player.fill(COLOR_WITHE)
+player_speed = [1, 1]
+
+player_rect = player.get_rect()
 
 playing = True
 
 while playing:
+    FPS.tick(120)
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
+    
+    main_display.fill(BLACK)
+
+    if player_rect.bottom >= HEIGHT or player_rect.top < 0:
+        player_speed[1] = -player_speed[1]
+
+    if player_rect.right >= WIDTH or player_rect.left < 0:
+        player_speed[0] = -player_speed[0]
+    
+    main_display.blit(player, player_rect)
+
+    player_rect = player_rect.move(player_speed)
+    pygame.display.flip()
